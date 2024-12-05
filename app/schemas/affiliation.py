00f4a23818f6b66fa680f8434e5161e4a1bdb3e8
Will Usher <wusher@kth.se>
2024-12-05 16:30:05 +0200

    Added affiliation schema

---
+++ app/schemas/affiliation.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from .author import AuthorBase
+
+class AffiliationModel(BaseModel):
+    id: Optional[str] = None
+    name: Optional[str] = None
+    ror: Optional[str] = None
+    ccg_partner: bool
