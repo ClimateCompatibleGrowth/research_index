a4e71358119e10e328f0a8fa134dfea96930dd3f
Will Usher <wusher@kth.se>
2024-12-05 15:45:53 +0200

    Updated schema for outputs

--- app/schemas/output.py
+++ app/schemas/output.py
@@ -2,12 +2,10 @@ from pydantic import BaseModel, Field, HttpUrl
 from datetime import datetime
 from typing import List, Optional
 from uuid import UUID
+from author import AuthorBase
+from country import CountryNodeModel
+from meta import Meta
 
-class Author(BaseModel):
-    uuid: UUID
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
-    orcid: Optional[HttpUrl] = None
 
 class DateTimeComponents(BaseModel):
     _Date__ordinal: Optional[int] = None
@@ -28,15 +26,56 @@ class CitedByDateTime(BaseModel):
     _DateTime__time: Optional[TimeComponents] = None
 
 class OutputModel(BaseModel):
+    """Schema for an Output
+
+    An output should look like this:
+
+    ```json
+        {
+        "uuid": "f05b1fc5-f831-4755-966f-06de074ab51c", # required
+        "doi": "http://doi.org/10.5281/zenodo.7015450", # required
+        "title": 'An example title', # required
+        "abstract": 'A long abstract', # optional
+        "journal": "Applied JSON", # optional
+        "cited_by_count_date": '2024-03-01', # optional
+        "cited_by_count": 34, # optional
+        "openalex": "https://openalex.org/W4393416420", # optional
+        "publication_day": 03, # optional
+        "publication_month": 12, # optional
+        "publication_year": 2023, # optional
+        "publisher": "Elsevier", # optional
+        "result_type": 'publication', # required
+        "countries": ['KEN', 'BEN'], # optional
+        "author": [
+                    {
+                    "uuid": "c613b25b-967c-4586-9101-ece3a901fd9c",
+                    "first_name": "Will",
+                    "last_name": "Usher",
+                    "orcid": "https://orcid.org/0000-0001-9367-1791",
+                    }
+                    ] # required
+        }
+    ```
+
+    """
     uuid: UUID
+    doi: HttpUrl
+    title: str
+    result_type: str
+    authors: List[AuthorBase]
     abstract: Optional[str] = None
+    journal: Optional[str] = None # Only academic publications have a journal
     cited_by_count_date: Optional[CitedByDateTime] = None
-    doi: Optional[str] = None
+    cited_by_count: Optional[int]
     publication_day: Optional[int] = None
     publication_month: Optional[int] = None
     publication_year: Optional[int] = None
     publisher: Optional[str] = None
-    result_type: Optional[str] = None
-    title: Optional[str] = None
-    countries: List = Field(default_factory=list)
-    authors: Optional[List[Author]] = Field(default_factory=list)
+    countries: Optional[List[CountryNodeModel]] = Field(default_factory=list)
+
+class OutputListModel(BaseModel):
+    """Represents a list of outputs including metadata
+
+    """
+    meta: Meta
+    outputs: List[OutputModel]
