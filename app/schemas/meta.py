5159b3c7bc50c70e806fff5ee61c81fafa80f02e
Will Usher <wusher@kth.se>
2024-12-05 15:46:14 +0200

    Added schema for outputs metadata

---
+++ app/schemas/meta.py
@@ -0,0 +1,39 @@
+from typing import Dict, List, Optional
+
+from pydantic import BaseModel, Field
+
+class Count(BaseModel):
+    """Represents count of the outputs
+    ```json
+        {'total': 245684392,
+        'publications': 1234,
+        'tools': 5678,
+        'dataset': 1234,
+        'other': 39494},
+     ```
+    """
+
+    total: int
+    publications: int
+    tool: int
+    dataset: int
+    other: int
+
+
+class Meta(BaseModel):
+    """
+    Base data model representing an academic author or contributor
+    with their associated metadata.
+
+    ```json
+    "count": {}
+    "db_response_time_ms": 929,
+    "page": 1,
+    "per_page": 25
+    ```
+
+    """
+    count: Count
+    db_response_time_ms: int
+    page: int
+    per_page: int
