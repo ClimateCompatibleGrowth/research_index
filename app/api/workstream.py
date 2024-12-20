64fd16b1ce6600a9cb49e89e1ae1c86a710204d5
Will Usher <wusher@kth.se>
2024-12-20 08:31:15 +0100

    Add workstream endpoints

--- app/api/workstream.py
+++ app/api/workstream.py
@@ -3,17 +3,18 @@ from typing import List
 from fastapi import APIRouter, HTTPException
 
 from app.crud.workstream import Workstream
-from app.schemas.workstream import WorkstreamBase, WorkstreamModel
+from app.schemas.workstream import WorkstreamDetailModel, WorkstreamListModel
 
 router = APIRouter(prefix="/api/workstreams", tags=["workstreams"])
 
 
 @router.get("")
-def list_workstreams() -> List[WorkstreamBase]:
+def list_workstreams(skip: int = 0, limit: int = 20) -> WorkstreamListModel:
     model = Workstream()
-    return model.get_all()
+    return model.get_all(skip=skip, limit=limit)
 
 
 @router.get("/{id}")
-def get_workstream(id: str) -> WorkstreamModel:
-    raise NotImplementedError("Implementation of workstream and author relationships is not yet complete")
+def get_workstream(id: str, skip: int = 0, limit: int = 20) -> WorkstreamDetailModel:
+    model = Workstream()
+    return model.get(id, skip=skip, limit=limit)
